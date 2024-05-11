eb6085fc89d95eb2dadd8bea2866af4b9e5be3b4
Julio Astudillo <julioastudillo3150@gmail.com>
2024-05-11 05:32:45 -0400

    fixed

--- JS/tickets.js
+++ JS/tickets.js
@@ -2,26 +2,40 @@ const GENERAL_ADMISSION_TICKET_COST = 20;
 const CHILD_AND_SENIOR_TICKET_COST = 10;
 const MATINEE_DISCOUNT = 3;
 
-function getBaseTicketCost(age) {
+// function getBaseTicketCost(age) {
     
-    if ((age <= 12) || (age >= 65)) {
-        cost = CHILD_AND_SENIOR_TICKET_COST;
-    } else {        
-            return GENERAL_ADMISSION_TICKET_COST;
-        }
+//     if ((age <= 12) || (age >= 65)) {
+//         cost = CHILD_AND_SENIOR_TICKET_COST;
+//     } else {        
+//             return GENERAL_ADMISSION_TICKET_COST;
+//         }
 
 
-};
+// };
 
 function buyTicket() {
     const age = prompt('What is your age?');
-    let cost = getBaseTicketCost(age)
-    
-    // const isMatinee = prompt('Are you attending a matinee show?');
-    
-    // if ((isMatinee === 'yes') || (isMatinee === 'y')) {
-    //     cost = cost - MATINEE_DISCOUNT;
-    // }
+    console.log(age);
+    let cost = getBaseTicketCost(age);
+    console.log("base cost: " + cost);
 
+    const isMatinee = prompt('Are you attending a matinee show?').toLowerCase();
+    console.log(isMatinee);
+    cost = applyMatineeDiscount(cost, isMatinee);
+    
     alert('Your ticket will cost: $' + cost);
+}
+function getBaseTicketCost(age){
+    if ((age <= 12) || (age >=65)) {
+        return CHILD_AND_SENIOR_TICKET_COST;
+    }
+    return GENERAL_ADMISSION_TICKET_COST;
+}
+
+function applyMatineeDiscount(cost, isMatinee) {
+    if ((isMatinee === 'yes')|| (isMatinee === 'y')) {
+        console.log("apply discount");
+        return cost - MATINEE_DISCOUNT;
+        }
+    return cost;
 }
